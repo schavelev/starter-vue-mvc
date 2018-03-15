85202789cc922688abe03f901e16dfc6f0bc2f78
Eduard Schavelev <e.shchavelev@gmail.com>
2018-03-15 17:24:17 +0700

    clone project from TryVueMvc to TryVueMvcGrid

---
+++ TryVueMvcGrid/ClientApp/components/Hello.ts
@@ -0,0 +1,25 @@
+﻿// ClientApp/components/Hello.ts
+import Vue from "vue";
+
+export default Vue.extend({
+    template:'#hello-template',
+    props: ['name', 'initialEnthusiasm'],
+    data() {
+        return {
+            enthusiasm: this.initialEnthusiasm
+        }
+    },
+    methods: {
+        increment() { this.enthusiasm++; },
+        decrement() {
+            if (this.enthusiasm > 1) {
+                this.enthusiasm--;
+            }
+        },
+    },
+    computed: {
+        exclamationMarks(): string {
+            return Array(this.enthusiasm + 1).join('!');
+        }
+    }
+});
